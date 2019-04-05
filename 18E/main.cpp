168d42a41c66111d7c785d12320236e32b2714a0
Kenny Chavez <kennychaveez@gmail.com>
2019-04-05 16:03:42 -0600

    UPDATED: Exercise 18, ready

--- 18E/main.cpp
+++ 18E/main.cpp
@@ -1,13 +1,17 @@
 /**
 **  Author: Kenny Chavez
-**  Desctiption: Ejercicio 1, suma, resta, multiplicacion, division y mod
+**  Desctiption: Ejercicio 18,
 **/
 
 #include <iostream>
 using namespace std;
 
-int main() 
+int main()
 {
-    cout << "Hello, World!";
+    double years, seg = 60 * 60 * 24 * 365;
+    cout << "Edad (years old): ";
+    cin >> years;
+    cout << "\nSegundos: " << seg * years;
+
     return 0;
 }
