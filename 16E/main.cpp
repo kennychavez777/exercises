ebe8c96d3bf6559814e3c6b6f3939c096690a684
Kenny Chavez <kennychaveez@gmail.com>
2019-04-10 19:51:45 -0600

    UPDATED: Exercise 16 and 19, ready

--- 16E/main.cpp
+++ 16E/main.cpp
@@ -1,13 +1,28 @@
 /**
 **  Author: Kenny Chavez
-**  Desctiption: Ejercicio 16
+**  Desctiption: Ejercicio 16, distancia entre 2 puntos
 **/
 
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 int main()
 {
-    cout << "Hello, World!";
+    double x, y, a, b, result; // Declaración de variables a utilizar en todo el flujo del programa
+
+    cout << "x: "; // Se pide x
+    cin >> x;
+
+    cout << "y: "; // Se pide y
+    cin >> y;
+
+    cout << "A: "; // Se pide A
+    cin >> a;
+    cout << "B: "; // Se pide B
+    cin >> b;
+
+    result = sqrt(pow(((b * y) - (a * y)), 2) + pow(((b * x) - (a * x)), 2)); // Se calcula el resultado de la ecuación
+    cout << "Respuesta: " << result; // Se muestra el resultado del cálculo
     return 0;
 }
--- 19E/main.cpp
+++ 19E/main.cpp
@@ -1,13 +1,34 @@
 /**
 **  Author: Kenny Chavez
-**  Desctiption: Ejercicio 1, suma, resta, multiplicacion, division y mod
+**  Desctiption: Ejercicio 19, calculo de la cuata respectiva de una casa
 **/
 
 #include <iostream>
+#include <math.h>
 using namespace std;
 
-int main() 
+int main()
 {
-    cout << "Hello, World!";
+    double house, i, years, r, m; // Se declaran las variables que se utilizaran en todo el flujo del programa
+
+    cout << "Valor de la casa: Q. "; // Se pide el valor de la casa
+    cin >> house;
+
+    cout << "Interes: "; // Se pide el interes
+    cin >> i;
+
+    if(i >= 100){ // Se evalua si el interes es mayor o igual al 100%
+        cout << "\nEl interes debe ser menor al 100%"; // Si es así se muestra un mensaje y se finaliza el programa
+        return 0;
+    }
+
+    cout << "Plazo (Years): "; // Se pide el plazo a pagar
+    cin >> years;
+
+    r = i / (100 / 12); // Se hace el calculo de r
+    m = ((house * r) / (1 - pow((1 + r), -12 * years))); // Se hace el calculo de m
+
+    cout << "\nRespuesta: " << house + m; // Se muestra el calculo
+
     return 0;
 }
